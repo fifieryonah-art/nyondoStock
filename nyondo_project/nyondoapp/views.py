303e5544698419be07d21350c8d99b3653208d58
fifieryonah-art <fifieryonah@gmail.com>
2026-04-10 06:59:37 +0300

    my nyondo project

---
+++ nyondo_project/nyondoapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def creditsales(request):
+    return render(request,'creditsales.html')
+
+def dashboard(request):
+    return render(request,'dashboard.html')
+
+def salesregistration(request):
+    return render(request,'salesregistration.html')
+
+def stockregistration():
+    (request,'stockregistration')
